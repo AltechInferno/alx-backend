4fed7196c433be2415dac457f46591fef9e9ff75
AltechInferno <altechinferno@gmail.com>
2024-01-28 10:04:17 +0000

    task1

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Simple pagination.
+"""Hypermedia pagination
 """
 import csv
 import math
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """gets the index range from a given page and page size.
+    """gets index range from a given page and page size.
     """
 
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
-    """class to paginate a database of popular baby names.
+    """class to paginate a db of popular baby names.
     """
-    FILE = "Popular_Baby_Names.csv"
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
+
     def dataset(self) -> List[List]:
-        """dataset
+        """Our cached dataset
         """
         if self.__dataset is None:
-            with open(self.FILE) as df:
+            with open(self.DATA_FILE) as df:
                 reader = csv.reader(df)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
@@ -41,3 +42,19 @@ class Server:
         if start > len(p_data):
             return []
         return p_data[start:end]
+
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """gathers information about a page.
+        """
+        p_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': p_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages
+        }
